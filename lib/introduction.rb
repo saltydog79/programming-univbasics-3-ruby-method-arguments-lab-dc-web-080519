d0e8761818cc3e77a50c924d6d712a7916368f5a
Emily <aspentree79@gmail.com>
2019-07-16 18:11:09 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,4 +10,4 @@ def introduction_with_language_optional(name, language)
 puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language_optional(name, "Ruby")
+introduction_with_language_optional(name, language)
